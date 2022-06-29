e9e4b60ffe94fd8f86a5f7177fdfff194292e756
Windows 10 <irobatic28@gmail.com>
2022-06-29 08:19:19 +0300

    acesta este un comit doi pentru "Prima Aplicatie3"

--- src/PrimaAplicatie3.java
+++ src/PrimaAplicatie3.java
@@ -1,5 +1,5 @@
 public class PrimaAplicatie3 {
     public static void main(String[] args) {
-        System.out.println("salut incerc ");
+        System.out.println("salut incerc sa invat java ");
     }
 }
