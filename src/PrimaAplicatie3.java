7ce30258934e04129c15558334104c1882259bf5
Windows 10 <irobatic28@gmail.com>
2022-06-30 06:48:33 +0300

    comentarii pe o singura liniie

--- src/PrimaAplicatie3.java
+++ src/PrimaAplicatie3.java
@@ -1,5 +1,8 @@
 public class PrimaAplicatie3 {
+    //acesta este o aplicatie
     public static void main(String[] args) {
+        //aceasta este o linie de code care afiseaza ceva
+        // new
         System.out.println("salut incerc sa invat java ");
     }
 }
