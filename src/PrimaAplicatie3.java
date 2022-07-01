ba6ed9a33b1f7323d7567cb9ff41dbd3d66512fa
Windows 10 <irobatic28@gmail.com>
2022-07-01 07:16:04 +0300

    comentarii pe mai multe linii

--- src/PrimaAplicatie3.java
+++ src/PrimaAplicatie3.java
@@ -1,8 +1,11 @@
 public class PrimaAplicatie3 {
-    //acesta este o aplicatie
-    public static void main(String[] args) {
+    /*
+    aici este un exemplu de comentariu pe mai multe linii
+     */
+
+    public static void main(/*poti pune si aici comentarii dar este incomod de citit codul*/String[] args) {
         //aceasta este o linie de code care afiseaza ceva
-        // new
+
         System.out.println("salut incerc sa invat java ");
     }
 }
