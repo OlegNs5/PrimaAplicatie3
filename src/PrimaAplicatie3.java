1b10cdccd03666237904c33463fd26f78ee07bb2
Windows 10 <irobatic28@gmail.com>
2022-07-04 07:37:51 +0300

    package and import package in your project

--- src/PrimaAplicatie3.java
+++ src/PrimaAplicatie3.java
@@ -3,11 +3,13 @@ public class PrimaAplicatie3 {
     /**
      *acesta este un javaDoc
      *acesta este un javaDoc
-     *@param args argument pentru args
+     *@param args argument pentru argsC:\Users\Windows 10\IdeaProjects\CtrlN-code\PrimaAplicatie3\src\misc
       * @author Macarici Oleg
      */
     public static void main(String[] args) {
 
         System.out.println("salut incerc sa invat java ");
+        ro.java.misc.main.main(args);
+        ro.java.ctrln.main.main(args);
     }
 }
