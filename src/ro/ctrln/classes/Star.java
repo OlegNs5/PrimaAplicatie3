a2e93f9301b9e448b07ace5502958032f4f335c4
Windows 10 <irobatic28@gmail.com>
2022-07-11 06:46:33 +0300

    commit pentru Star.java

--- src/ro/ctrln/classes/Star.java
+++ src/ro/ctrln/classes/Star.java
@@ -3,10 +3,13 @@ package ro.ctrln.classes;
 import java.math.BigDecimal;
 
 public class Star {
-    private String StarDiscription;
-    private int diameter;
-    private int satellites;
-    private BigDecimal mass;
+    private String StarDiscription; //variabile de instanta(dar sa nu fie tip "static")
+    private int diameter; //variabile de instanta(dar sa nu fie tip "static")
+    private int satellites; //variabile de instanta(dar sa nu fie tip "static")
+    private BigDecimal mass; //variabile de instanta(dar sa nu fie tip "static")
+    private String planetNane; //variabile de instanta(dar sa nu fie tip "static")
+
+public static final boolean MIKYWAYSTAR = true; //declararea constantelor in java/se refera la "classa public star"
 
     public Star(){}
                          //generare constructor din Alt+Insert sau meniul "Code"
@@ -17,35 +20,48 @@ public class Star {
         this.mass = mass;
     }
                         //generare get si set din Alt+Insert sau din  meniul
+
     public String getStarDiscription() {
         return StarDiscription;
-    }
-
-    public void setStarDiscription(String StarDiscription) {
-        this.StarDiscription = StarDiscription;
     }
 
     public int getDiameter() {
         return diameter;
     }
 
-    public void setDiameter(int diameter) {
-        this.diameter = diameter;
-    }
-
     public int getSatellites() {
         return satellites;
-    }
-
-    public void setSatellites(int satellites) {
-        this.satellites = satellites;
     }
 
     public BigDecimal getMass() {
         return mass;
     }
 
+    public String getPlanetNane() {
+        return planetNane; //variabila de instanta
+    }
+
+    public void setStarDiscription(String starDiscription) {
+        StarDiscription = starDiscription;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public void setSatellites(int satellites) {
+        this.satellites = satellites;
+    }
+
     public void setMass(BigDecimal mass) {
         this.mass = mass;
+    }
+
+    public void setPlanetNane(String planetNane) {
+        this.planetNane = planetNane; //variabila de instanta
+    }
+
+    class smallPlanet{
+        private String planetNane;
     }
 }
